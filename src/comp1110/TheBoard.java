77b95c0ee7034e594a2ed4a6ddd2cfc3e919f8b3
AdiAr11 <aditya2000arora@gmail.com>
2024-04-04 22:57:36 +1100

    Good progress on Task 4, drawing the board

---
+++ src/comp1110/TheBoard.java
@@ -0,0 +1,48 @@
+package comp1110;
+
+public class TheBoard {
+    private char[][] squares;
+    int rows;
+    int columns;
+
+    // Constructor
+    public TheBoard(String boardString) {
+        String[] list = boardString.split("\\r?\\n");
+
+        rows = list.length;
+        columns = list[0].length();
+        squares = new char[rows][columns];
+
+        for (int i = 0; i < rows; i++) {
+            String rowString = list[i];
+            for (int j = 0; j < columns; j++) {
+                squares[i][j] = rowString.charAt(j);
+            }
+        }
+    }
+
+    public char[][] getSquares() {
+        return squares;
+    }
+
+    public int getRows(){
+        return rows;
+    }
+
+    public int getColumns(){
+        return columns;
+    }
+
+    public char getColor(int row, int column) {
+        return squares[row][column];
+    }
+
+    // Check if a square has a cat on it
+    public boolean hasCat(int row, int column) {
+        char square = squares[row][column];
+        return Character.isUpperCase(square);
+    }
+
+
+}
+
--- src/comp1110/ass2/gui/Viewer.java
+++ src/comp1110/ass2/gui/Viewer.java
@@ -1,13 +1,20 @@
 package comp1110.ass2.gui;
 
+import comp1110.TheBoard;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+
+import java.util.Arrays;
+import java.util.Random;
 
 public class Viewer extends Application {
 
@@ -28,7 +35,80 @@ public class Viewer extends Application {
      *
      */
     void displayState(String boardstate, String hand) {
+
+//        root.getChildren().clear(); // Clear previous state
+//        root.getChildren().add(controls); // Add controls back
+
+        String testingBoardState = """
+                 fffffffffrrfffffff
+                 fffffffffrRfffffff
+                 fffffffffrrfffffff
+                 fffgffyrgpygyrygbr
+                 fffgGfggyygprbprpg
+                 fffgggbgprbpygbpyb
+                 ffffffbpbpgrbrrbgy
+                 ffffffgygybpgygprb
+                 ffffffbrrrybgygybg
+                 ffffffgpbbyrprgbbp
+                 ffffffbyrbpybgpryg
+                 ffffffbyrbpybgpryg
+                 ffffffpgyrggrbgyby
+                 fffffybgbpryybpgyp
+                 ffffYyybpgbprygrow
+                 fffyyyyryygbygybww
+                 """;
+
+        TheBoard theBoard = new TheBoard(testingBoardState);
+
+        System.out.println("Namaste Mummy and Papa!! " + theBoard.getRows() + " : " + theBoard.getColumns());
+
+        GridPane gridPane = new GridPane();
+        gridPane.setHgap(2);
+        gridPane.setVgap(2);
+
+        for (int i = 0; i < theBoard.getRows(); i++) {
+
+            for (int j = 0; j < theBoard.getColumns(); j++) {
+
+
+                Rectangle rectangle = new Rectangle(35, 35);
+
+                if(theBoard.hasCat(i,j)){
+
+                }
+
+                checkSquareColour(theBoard, rectangle, i, j);
+
+
+                gridPane.add(rectangle, j, i);
+            }
+        }
+
+        gridPane.setLayoutX(380);
+
+        root.getChildren().add(gridPane);
+
         // FIXME TASK 4
+    }
+
+    void checkSquareColour(TheBoard theBoard, Rectangle rectangle, int i, int j){
+        if (theBoard.getColor(i,j) == 'f') {
+            rectangle.setFill(Color.DARKORANGE);
+        } else if (theBoard.getColor(i,j) == 'b') {
+            rectangle.setFill(Color.BLUE);
+        } else if (theBoard.getColor(i,j) == 'r') {
+            rectangle.setFill(Color.RED);
+        }
+        else if (theBoard.getColor(i,j) == 'y') {
+            rectangle.setFill(Color.YELLOW);
+        }
+        else if (theBoard.getColor(i,j) == 'p') {
+            rectangle.setFill(Color.PURPLE);
+        }else if (theBoard.getColor(i,j) == 'g') {
+            rectangle.setFill(Color.GREEN);
+        }else{
+            rectangle.setFill(Color.BLACK);
+        }
     }
 
     /**
