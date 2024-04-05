869483170b334bba5e39555c031e9c5fec864321
Yu Ma <u7670957@anu.edu.au>
2024-04-05 14:20:53 +1100

    D2C task5 and firetile/square constructor

--- src/comp1110/ass2/FireTile.java
+++ src/comp1110/ass2/FireTile.java
@@ -8,65 +8,72 @@ public class FireTile {
     //fields
     char fireID;
     Location mainLocation;
-    Square[][] fireSquares;
+    Square[] fireSquares;
     List<Location> coordinates;
+    String info;
 
     public FireTile(){}
 
+
     // constructor use fromString to get coordinates
-    public FireTile(char id, List<Location> coordinates) {
-        this.fireID = id;
-        this.coordinates = coordinates;
-        this.fireSquares = new Square[9][9];
+    public FireTile(char fireID) {
+        this.fireID = fireID;
+//        System.out.println(this.fireID);
+        if (fireID<='z' && fireID>='a'){
+            info = Utility.FIRE_TILES[fireID-'a'];
+        }else info = Utility.FIRE_TILES[fireID-'A'+26];//"w0010111222"
+
+        this.fireSquares = new Square[(info.length()-1)/2];// 这个fireTile一共有多少个square
+        for (int i = 1; i < this.fireSquares.length; i+=2) {
+            String location = info.substring(i,i+2);
+            this.fireSquares[i-1] = new Square(location,Colour.fromChar('f'));
+        }
+//        System.out.println(this.fireSquares[2].location);
     }
 
-    //constructor to initialise
-    public FireTile(String[] fireStrArray) {
-        //fromString uses the first constructor "public FireTile(char id, List<Location> coordinates)"
-        List<FireTile> fireTileList = fromString(fireStrArray);
-//        for (FireTile fireTile : fireTileList) {
-//            System.out.println(fireTile);
-//        }
-//        System.out.println("==============");
-//        System.out.println(fireTileList.get(4));
-//        System.out.println(fireTileList.get(4).fireSquares[1][1].getcolour());
-//        return fireTileList;
-    }
+
 
     // print something like "a000110111221"
     public String toString() {
         StringBuilder fireStr = new StringBuilder();
-        fireStr.append(fireID);
-        for (Location coordinate : coordinates) {
-            fireStr.append(coordinate);
+
+        fireStr.append(this.fireID);
+//        System.out.println("toString: "+ this.fireID);
+//        System.out.println("toString: "+ this.fireSquares[2].location);
+        for (Square fireSquare : this.fireSquares) {
+            if (fireSquare != null) { //********** Check if fireSquare is not null
+                fireStr.append(fireSquare.location);
+//                System.out.println(fireStr);
+            }
         }
+//        System.out.println(fireStr);
         return fireStr.toString();
     }
 
     // convert str to fire tile, return a List<FireTile> that as 31 elements.
     // Index start from 0, fireID start from a to E
-    public static List<FireTile> fromString(String[] fireStrArray) {
-        List<FireTile> result = new ArrayList<>();
-        for (String fs : fireStrArray) {
-            char id = fs.charAt(0);
-            List<Location> coordinates = new ArrayList<>();
-            Square[][] fireSquares = new Square[9][9];
-
-            for (int i = 1; i < fs.length(); i += 2) {
-                int row = Character.getNumericValue(fs.charAt(i));
-                int column = Character.getNumericValue(fs.charAt(i + 1));
-                fireSquares[row][column] = new Square();
-                fireSquares[row][column].setColour(Colour.fromChar('f'));
-                Location location = new Location(row, column);
-                coordinates.add(location);
-            }
-
-            FireTile fireTile = new FireTile(id, coordinates);
-            fireTile.fireSquares = fireSquares;
-            result.add(fireTile);
-        }
-        return result;
-    }
+//    public static List<FireTile> fromString(String[] fireStrArray) {
+//        List<FireTile> result = new ArrayList<>();
+//        for (String fs : fireStrArray) {
+//            char id = fs.charAt(0);
+//            List<Location> coordinates = new ArrayList<>();
+//            Square[][] fireSquares = new Square[9][9];
+//
+//            for (int i = 1; i < fs.length(); i += 2) {
+//                int row = Character.getNumericValue(fs.charAt(i));
+//                int column = Character.getNumericValue(fs.charAt(i + 1));
+//                fireSquares[row][column] = new Square();
+//                fireSquares[row][column].setColour(Colour.fromChar('f'));
+//                Location location = new Location(row, column);
+//                coordinates.add(location);
+//            }
+//
+//            FireTile fireTile = new FireTile(id, coordinates);
+//            fireTile.fireSquares = fireSquares;
+//            result.add(fireTile);
+//        }
+//        return result;
+//    }
 
 
 
