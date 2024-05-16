19b3466b30875c047b5471d631d901209ab9b7fa
AdiAr11 <aditya2000arora@gmail.com>
2024-05-16 15:38:25 +1000

    Made Card objects instead of string manipulation

--- src/comp1110/ass2/Card.java
+++ src/comp1110/ass2/Card.java
@@ -1,5 +1,10 @@
 package comp1110.ass2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 // author: Yu Ma
 public class Card {
     /*
@@ -15,6 +20,18 @@ public class Card {
 
     char[][] cardColours = new char[3][3];
 
+    //easier approach for colors
+    private String colors;
+
+    public Card(String cardString) {
+        this.cardId = cardString.charAt(0);
+        this.colors = cardString.substring(1);
+    }
+
+    public String getColors() {
+        return colors;
+    }
+
     public Card(){}
     public Card(char deckId, char cardId, String pattern) {
         this.deckId = deckId;
@@ -22,6 +39,19 @@ public class Card {
         setCardColours(pattern);
     }
 
+    public static List<Card> convertStringArrayOfCardsToListOldApproach(String[] deck) {
+        List<Card> cardList = new ArrayList<>();
+        for (String cardString : deck) {
+            cardList.add(new Card(cardString));
+        }
+        return cardList;
+    }
+
+    public static List<Card> convertStringArrayOfCardsToList(String[] deck) {
+        return Arrays.stream(deck)
+                .map(Card::new) // Convert each string to a Card object
+                .collect(Collectors.toList()); // Collect results to a list
+    }
 
     private void setCardColours(String pattern) {
         String[] rows = pattern.split("(?<=\\G.{9})"); // Split string every 9 characters
